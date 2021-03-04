b413ef2cc0fa99c2d62ab386835ce503a248e536
Jake Elizondo <jake.elizondo23@gmail.com>
2021-03-04 16:28:04 -0500

    REGISTER ROUTE
    created route, added registration form and connected hooks to handle registration and any errors

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TEAMS } from '../../TEAMS';
+import AuthApiService from '../../services/auth-api-service';
+import ErrorModal from '../UI/ErrorModal/ErrorModal';
+import LoadingIndicator from '../UI/LoadingIndicator/LoadingIndicator';
+import './RegisterForm.css';
+
+const RegistrationForm = (props) => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredTeam, setEnteredTeam] = useState('');
+
+  const handleSubmit = (ev) => {
+    setIsLoading(true);
+    ev.preventDefault();
+
+    let user = {
+      username: enteredUsername,
+      password: enteredPassword,
+      name: enteredName,
+      team: enteredTeam,
+    };
+
+    AuthApiService.postUser(user)
+      .then((response) => {
+        setIsLoading(false);
+        setEnteredName('');
+        setEnteredUsername('');
+        setEnteredPassword('');
+        setEnteredTeam('');
+        props.onRegisterSuccess();
+      })
+      .catch((response) => {
+        setIsLoading(false);
+        setError(response.error.message);
+      });
+  };
+
+  const clear = () => {
+    setError(null);
+    setIsLoading(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="registration-form">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+      <div className="registration-input">
+        <label htmlFor="registration-name-input">Enter your name</label>
+        <input
+          id="registration-name-input"
+          name="name"
+          value={enteredName}
+          onChange={(e) => setEnteredName(e.target.value)}
+          required
+        />
+      </div>
+      <div className="registration-input">
+        <label htmlFor="registration-username-input">Choose a username</label>
+        <input
+          id="registration-username-input"
+          name="username"
+          value={enteredUsername}
+          onChange={(e) => setEnteredUsername(e.target.value)}
+          required
+        />
+      </div>
+      <div className="registration-input">
+        <label htmlFor="registration-password-input">Choose a password</label>
+        <input
+          id="registration-password-input"
+          name="password"
+          type="password"
+          value={enteredPassword}
+          onChange={(e) => setEnteredPassword(e.target.value)}
+          required
+        />
+      </div>
+      <div className="registration-input">
+        <label htmlFor="registration-team-select">
+          Select your favorite team
+        </label>
+        <select
+          id="registration-team-select"
+          name="favorite-team"
+          value={enteredTeam}
+          onChange={(e) => setEnteredTeam(e.target.value)}
+          required
+        >
+          <option></option>
+          {TEAMS.map((team) => (
+            <option key={team.teamcode} value={team.teamcode}>
+              {team.teamname}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button type="submit" className="red-back">
+        Sign up
+      </button>{' '}
+      {isLoading && <LoadingIndicator />}
+      <Link to="/login">Already have an account?</Link>
+    </form>
+  );
+};
+
+export default RegistrationForm;
--- src/routes/RegisterRoute/RegisterRoute.js
+++ src/routes/RegisterRoute/RegisterRoute.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
 
-const RegisterRoute = () => {
-  return <div>RegisterRoute</div>;
+const RegisterRoute = (props) => {
+  const handleRegisterAttempt = useCallback(() => {
+    props.history.push('/login');
+  }, [props.history]);
+
+  return (
+    <div className="register-page">
+      <RegisterForm onRegisterSuccess={handleRegisterAttempt} />
+    </div>
+  );
 };
 
 export default RegisterRoute;
