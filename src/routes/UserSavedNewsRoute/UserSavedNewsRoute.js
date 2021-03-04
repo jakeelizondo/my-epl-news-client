ce2f55488f12fff832b0f3e9cca2409b98ed7516
Jake Elizondo <jake.elizondo23@gmail.com>
2021-03-04 17:06:07 -0500

    removed unnecesssary import statements

--- src/routes/UserSavedNewsRoute/UserSavedNewsRoute.js
+++ src/routes/UserSavedNewsRoute/UserSavedNewsRoute.js
@@ -1,12 +1,4 @@
-import React, { useEffect } from 'react';
-import TokenService from '../../services/token-service';
-
 const UserSavedNewsRoute = (props) => {
-  // useEffect(() => {
-  //   if (!TokenService.hasAuthToken()) {
-  //     props.history.push('/login');
-  //   }
-  // }, [props.history]);
   return <div>UserSavedNewsRoute</div>;
 };
 
