8ce60390ff5e62bff35ee52a062a262a78c75e5d
Jake Elizondo <jake.elizondo23@gmail.com>
2021-03-05 16:33:07 -0500

    changed font
    changed orientation of elements in mobile filter bar

--- src/components/Mobile/NewsFilterBar/NewsFilterBar.js
+++ src/components/Mobile/NewsFilterBar/NewsFilterBar.js
@@ -10,22 +10,25 @@ const NewsFilterBar = (props) => {
         {props.team && <h3>Current Team: {props.team}</h3>}
       </div>
       <div className="filter-options">
-        <label>Select Team:</label>
-        <select
-          id="filter-bar-team-select"
-          name="filter-team"
-          value={filterTeam}
-          onChange={(e) => setFilterTeam(e.target.value)}
-          required
-        >
-          <option></option>
-          {TEAMS.map((team) => (
-            <option key={team.teamcode} value={team.teamcode}>
-              {team.teamname}
-            </option>
-          ))}
-        </select>
-        <button onClick={() => props.handleFilter(filterTeam)}>Filter</button>
+        <label htmlFor="filter-bar-team-select">Select Team:</label>
+
+        <div className="filter-inputs">
+          <select
+            id="filter-bar-team-select"
+            name="filter-team"
+            value={filterTeam}
+            onChange={(e) => setFilterTeam(e.target.value)}
+            required
+          >
+            <option></option>
+            {TEAMS.map((team) => (
+              <option key={team.teamcode} value={team.teamcode}>
+                {team.teamname}
+              </option>
+            ))}
+          </select>
+          <button onClick={() => props.handleFilter(filterTeam)}>Filter</button>
+        </div>
       </div>
     </div>
   );
