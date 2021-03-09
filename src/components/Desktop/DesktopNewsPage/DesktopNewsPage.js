8c8db85a2e27f38f9116ad37ead02768885b433a
Jake Elizondo <jake.elizondo23@gmail.com>
2021-03-09 11:02:04 -0500

    Error Handling
    Added catch statements and error handling to both desktop and mobile news components

--- src/components/Desktop/DesktopNewsPage/DesktopNewsPage.js
+++ src/components/Desktop/DesktopNewsPage/DesktopNewsPage.js
@@ -7,6 +7,7 @@ import ArticlesService from '../../../services/articles-service';
 import SoccerLoadingIndicator from '../../UI/SoccerLoadingIndicator/SoccerLoadingIndicator';
 import TokenService from '../../../services/token-service';
 import EmptyNewsMessage from '../../UI/EmptyNewsMessage/EmptyNewsMessage';
+import ErrorModal from '../../UI/ErrorModal/ErrorModal';
 import './DesktopNewsPage.css';
 
 const DesktopNewsPage = () => {
@@ -16,6 +17,7 @@ const DesktopNewsPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isEnd, setIsEnd] = useState(false);
   const [page, setPage] = useState();
+  const [error, setError] = useState(null);
 
   // if the user is already logged in, use their jwt token to pre-populate news page for them
   useEffect(() => {
@@ -25,13 +27,22 @@ const DesktopNewsPage = () => {
       setPage(1);
       setTeamCode(user.team);
       setIsLoading(true);
-      ArticlesService.getTeamArticles(user.team, page).then((res) => {
-        setIsLoading(false);
-        setTeam(TEAMKEY[user.team].teamcode);
-        setArticles(res);
-      });
+      ArticlesService.getTeamArticles(user.team, page)
+        .then((res) => {
+          setIsLoading(false);
+          setTeam(TEAMKEY[user.team].teamcode);
+          setArticles(res);
+        })
+        .catch((res) => {
+          setError(res.error.message);
+        });
     }
   }, [page]);
+
+  const clear = () => {
+    setError(null);
+    setIsLoading(false);
+  };
 
   const handleFilter = useCallback(
     (filterTeamCode) => {
@@ -39,11 +50,15 @@ const DesktopNewsPage = () => {
       setPage(1);
       setTeamCode(filterTeamCode);
       setIsLoading(true);
-      ArticlesService.getTeamArticles(filterTeamCode, page).then((res) => {
-        setIsLoading(false);
-        setTeam(TEAMKEY[filterTeamCode].teamcode);
-        setArticles(res);
-      });
+      ArticlesService.getTeamArticles(filterTeamCode, page)
+        .then((res) => {
+          setIsLoading(false);
+          setTeam(TEAMKEY[filterTeamCode].teamcode);
+          setArticles(res);
+        })
+        .catch((res) => {
+          setError(res.error.message);
+        });
     },
     [page]
   );
@@ -52,28 +67,37 @@ const DesktopNewsPage = () => {
     setIsLoading(true);
     setIsEnd(false);
 
-    ArticlesService.getTeamArticles(teamCode, page - 1).then((res) => {
-      setPage(page - 1);
-      setIsLoading(false);
-      setArticles(res);
-    });
+    ArticlesService.getTeamArticles(teamCode, page - 1)
+      .then((res) => {
+        setPage(page - 1);
+        setIsLoading(false);
+        setArticles(res);
+      })
+      .catch((res) => {
+        setError(res.error.message);
+      });
   }, [teamCode, page]);
 
   const handleNextClick = useCallback(() => {
     setIsLoading(true);
 
-    ArticlesService.getTeamArticles(teamCode, page + 1).then((res) => {
-      if (res.length < 1) {
-        setIsEnd(true);
-      }
-      setPage(page + 1);
-      setIsLoading(false);
-      setArticles(res);
-    });
+    ArticlesService.getTeamArticles(teamCode, page + 1)
+      .then((res) => {
+        if (res.length < 1) {
+          setIsEnd(true);
+        }
+        setPage(page + 1);
+        setIsLoading(false);
+        setArticles(res);
+      })
+      .catch((res) => {
+        setError(res.error.message);
+      });
   }, [teamCode, page]);
 
   return (
     <div className="desktop-news-page">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <div className="left-side">
         <TeamFilterSidebar handleFilter={handleFilter} currTeam={teamCode} />
       </div>
--- src/components/Mobile/MobileNewsPage/MobileNewsPage.js
+++ src/components/Mobile/MobileNewsPage/MobileNewsPage.js
@@ -6,6 +6,7 @@ import { TEAMKEY } from '../../../team-helpers/TEAMS';
 import ArticlesService from '../../../services/articles-service';
 import SoccerLoadingIndicator from '../../UI/SoccerLoadingIndicator/SoccerLoadingIndicator';
 import TokenService from '../../../services/token-service';
+import ErrorModal from '../../UI/ErrorModal/ErrorModal';
 import EmptyNewsMessage from '../../UI/EmptyNewsMessage/EmptyNewsMessage';
 
 const MobileNewsPage = () => {
@@ -15,6 +16,12 @@ const MobileNewsPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isEnd, setIsEnd] = useState(false);
   const [page, setPage] = useState();
+  const [error, setError] = useState(null);
+
+  const clear = () => {
+    setError(null);
+    setIsLoading(false);
+  };
 
   // if the user is already logged in, use their jwt token to pre-populate news page for them
   useEffect(() => {
@@ -24,11 +31,15 @@ const MobileNewsPage = () => {
       setPage(1);
       setTeamCode(user.team);
       setIsLoading(true);
-      ArticlesService.getTeamArticles(user.team, page).then((res) => {
-        setIsLoading(false);
-        setTeam(TEAMKEY[user.team].teamcode);
-        setArticles(res);
-      });
+      ArticlesService.getTeamArticles(user.team, page)
+        .then((res) => {
+          setIsLoading(false);
+          setTeam(TEAMKEY[user.team].teamcode);
+          setArticles(res);
+        })
+        .catch((res) => {
+          setError(res.error.message);
+        });
     }
   }, [page]);
 
@@ -37,11 +48,15 @@ const MobileNewsPage = () => {
       setPage(1);
       setTeamCode(filterTeamCode);
       setIsLoading(true);
-      ArticlesService.getTeamArticles(filterTeamCode, page).then((res) => {
-        setIsLoading(false);
-        setTeam(TEAMKEY[filterTeamCode].teamcode);
-        setArticles(res);
-      });
+      ArticlesService.getTeamArticles(filterTeamCode, page)
+        .then((res) => {
+          setIsLoading(false);
+          setTeam(TEAMKEY[filterTeamCode].teamcode);
+          setArticles(res);
+        })
+        .catch((res) => {
+          setError(res.error.message);
+        });
     },
     [page]
   );
@@ -50,28 +65,37 @@ const MobileNewsPage = () => {
     setIsLoading(true);
     setIsEnd(false);
 
-    ArticlesService.getTeamArticles(teamCode, page - 1).then((res) => {
-      setPage(page - 1);
-      setIsLoading(false);
-      setArticles(res);
-    });
+    ArticlesService.getTeamArticles(teamCode, page - 1)
+      .then((res) => {
+        setPage(page - 1);
+        setIsLoading(false);
+        setArticles(res);
+      })
+      .catch((res) => {
+        setError(res.error.message);
+      });
   }, [teamCode, page]);
 
   const handleNextClick = useCallback(() => {
     setIsLoading(true);
 
-    ArticlesService.getTeamArticles(teamCode, page + 1).then((res) => {
-      if (res.length < 1) {
-        setIsEnd(true);
-      }
-      setPage(page + 1);
-      setIsLoading(false);
-      setArticles(res);
-    });
+    ArticlesService.getTeamArticles(teamCode, page + 1)
+      .then((res) => {
+        if (res.length < 1) {
+          setIsEnd(true);
+        }
+        setPage(page + 1);
+        setIsLoading(false);
+        setArticles(res);
+      })
+      .catch((res) => {
+        setError(res.error.message);
+      });
   }, [teamCode, page]);
 
   return (
     <div className="mobile-news-page">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <NewsFilterBar handleFilter={handleFilter} team={team} />
       {isLoading && <SoccerLoadingIndicator />}
       {!team && <EmptyNewsMessage />}
